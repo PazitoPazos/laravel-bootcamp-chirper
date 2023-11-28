5826557c0e02c00eac6c50d64c48bb88b027646b
PazitoPazos <javier.pazos001@gmail.com>
2023-11-28 17:50:43 +0100

    Showing chirps

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -18,7 +18,7 @@ class ChirpController extends Controller
     public function index(): Response
     {
         return Inertia::render('Chirps/Index', [
-            //
+            'chirps' => Chirp::with('user:id,name')->latest()->get()
         ]);
     }
 
@@ -40,7 +40,7 @@ class ChirpController extends Controller
         ]);
 
         $request->user()->chirps()->create($validated);
- 
+
         return redirect(route('chirps.index'));
     }
 
